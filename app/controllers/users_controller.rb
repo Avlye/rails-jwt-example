c3ab3ab8b6d9ed966f22ae571958fa5c55652208
Gabriel Almir <avlye@outlook.com>
2020-07-24 10:28:05 -0300

    Inicial Commit

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,78 @@
+# frozen_string_literal: true
+
+# UserControllers provides JWT auth controller
+class UsersController < ApplicationController
+  before_action :authorized, only: [:auto_login]
+
+  def encode_token(payload)
+    JWT.encode(payload, 's3cr3t')
+  end
+
+  def auth_header
+    # { Authorization: 'Bearer <token>' }
+    request.headers['Authorization']
+  end
+
+  def decoded_token
+    if auth_header
+      token = auth_header.split(' ')[1]
+      # Header: { 'Authorization': 'Bearer <token>' }
+      begin
+        JWT.decode(token, 's3cr3t', true)
+      rescue JWT::DecodeError
+        nil
+      end
+
+    end
+  end
+
+  def logged_in_user
+    if decoded_token
+      user_id = decoded_token[0]['user_id']
+      @user = User.find_by(id: user_id)
+    end
+  end
+
+  def logged_in?
+    !!logged_in_user
+  end
+
+  def authorized
+    unless logged_in?
+      render json: { message: 'Please log in' },
+             status: :unauthorized
+    end
+  end
+
+  # REGISTER
+  def create
+    @user = User.create(user_params)
+
+    if @user.valid?
+      token = encode_token({ user_id: @user.id })
+      render json: { user: @user, token: token }
+    else
+      render json: { error: 'Invalid username or password' }
+    end
+  end
+
+  # Logging in
+  def login
+    @user = User.find_by(username: params[:username])
+
+    if @user&.authenticate(params[:password])
+      token = encode_token({ user_id: @user.id })
+      render json: { user: @user, token: token }
+    end
+  end
+
+  def auto_login
+    render json: @user
+  end
+
+  private
+
+  def user_params
+    params.permit(:username, :password, :age)
+  end
+end
